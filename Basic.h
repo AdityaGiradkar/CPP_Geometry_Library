6794e512e84d0d0786809a0403f85a8d03f0bbb2
Archit <archit.hadge@gmail.com>
2020-02-15 10:39:30 +0530

    Centroid,Perimeter and overloading <<operator

--- Basic.h
+++ Basic.h
@@ -1,15 +1,17 @@
-
 #include<math.h>
-
+#include<iostream>
+using namespace std;
 class Point          //Class for point
 {
 public:
     float x,y,z;
 
-    Point(){
-    this->x=0;
-    this->y=0;
-    this->z=0;}
+    Point()
+    {
+        this->x=0;
+        this->y=0;
+        this->z=0;
+    }
 
     Point(float x,float y,float z)
     {
@@ -30,6 +32,8 @@ public:
         return sqrt(x*x+y*y+z*z);
     }
 
+
+
 };
 
 float Distance(Point p1,Point p2)
@@ -38,30 +42,53 @@ float Distance(Point p1,Point p2)
 }
 
 
-class Triangle{
+class Triangle
+{
 public:
-Point p1,p2,p3;
+    Point p1,p2,p3;
 
-Triangle(){
-this->p1.x=0;
-this->p2.x=0;
-this->p3.x=0;
-this->p1.y=0;
-this->p2.y=0;
-this->p3.y=0;}
+    Triangle()
+    {
+        this->p1.x=0;
+        this->p2.x=0;
+        this->p3.x=0;
+        this->p1.y=0;
+        this->p2.y=0;
+        this->p3.y=0;
+    }
 
-Triangle(Point p1, Point p2, Point p3){
-this->p1.x=p1.x;
-this->p2.x=p2.x;
-this->p3.x=p3.x;
-this->p1.y=p1.y;
-this->p2.y=p2.y;
-this->p3.y=p3.y;}
+    Triangle(Point p1, Point p2, Point p3)
+    {
+        this->p1.x=p1.x;
+        this->p2.x=p2.x;
+        this->p3.x=p3.x;
+        this->p1.y=p1.y;
+        this->p2.y=p2.y;
+        this->p3.y=p3.y;
+    }
 
-float area(){
-float a=0.5*(p1.x*(p2.y-p3.y)+p2.x*(p3.y-p1.y)+p3.x*(p1.y-p2.y));
-return a;}
+    float area()
+    {
+        float a=0.5*(p1.x*(p2.y-p3.y)+p2.x*(p3.y-p1.y)+p3.x*(p1.y-p2.y));
+        if(a<0){
+        return -a;}
+        return a;
+    }
+
+    float perimeter(){
+    return Distance(p1,p2)+Distance(p2,p3)+Distance(p1,p3);}
+
+    Point centroid(){
+    Point p;
+    p.x=(p1.x+p2.x+p3.x)/3;
+    p.y=(p1.y+p2.y+p3.y)/3;
+    return p;
+    }
 };
+
+ostream & operator<<(ostream &out,const Point &p){
+    out<<"("<<p.x<<","<<p.y<<")";
+    return out;}
 
 
 
--- test.cpp
+++ test.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
-Point p1(0,0),p2(1,1),p3(1,1);
+Point p1(0,0),p2(0,10),p3(10,0);
 Triangle t1(p1,p2,p3);
-cout<<Distance(p1,p2)<<" "<<t1.area();}
+cout<<t1.centroid();
+cout<<Distance(p1,p2)<<" "<<t1.area()<<" "<<t1.perimeter();}
